ce7e02db687ba5b389e74382e4b4ac209b59068e
Jamcda <jamcdaniels99@gmail.com>
2025-10-05 12:58:28 -0400

    added google cloud dependencies

--- clear-sky-backend/src/test/java/com/example/clear_sky_backend/ClearSkyBackendApplicationTests.java
+++ clear-sky-backend/src/test/java/com/example/clear_sky_backend/ClearSkyBackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.clear_sky_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClearSkyBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
